c7ee8624669563f958318b8be39526cf3edf95c2
Muaz Sikiric <muaaz.sikiric@gmail.com>
2024-02-10 13:49:29 +0100

    Add pixelated trail effect

--- script.js
+++ script.js
@@ -3,5 +3,9 @@ const container = document.querySelector("#container");
 for (let i = 0; i < 256; i++) {
   const square = document.createElement("div");
   square.classList.add("square");
+  square.addEventListener("mouseenter", (e) => {
+    square.classList.add("color");
+  });
+
   container.appendChild(square);
 }
